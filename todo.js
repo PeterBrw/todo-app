ac8c9439200c58963d0f46984997938c5652667b
Petru <peter.giurgiu@gmail.com>
2019-02-11 16:56:02 +0200

    41-the-for-loop

--- todo.js
+++ todo.js
@@ -10,3 +10,9 @@ todos.forEach(function(todo, index) {
   const num = index + 1
   console.log(`${num}. ${todo}`)
 })
+
+
+for(let count = 0; count < todos.length; count++) {
+  const num = count + 1
+  console.log(`${num}. ${todos[count]}`)
+}

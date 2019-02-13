85b2dcb1ad92ee8f51deacf7de041e90b3d32e8f
Petru <peter.giurgiu@gmail.com>
2019-02-13 17:33:20 +0200

    52-adding-elements-via-the-dom

--- todo.js
+++ todo.js
@@ -15,39 +15,71 @@ const todos = [{
   completed: true
 }]
 
+let left = 0
 
-const sortTodos = function(todos) {
-  todos.sort(function(a, b) {
-    if(!a.completed && b.completed) {
-      return -1
-    } else if(!b.completed && a.completed) {
-      return 1
-    } else {
-      return 0
-    }
-  })
-}
-
-const deleteTodo = function(todos, todoText) {
-  const index = todos.findIndex(function(todo){
-    return todo.text.toLowerCase() === todoText.toLowerCase()
-  })
-  
-  if(index > -1) {
-    todos.splice(index, 1);
+todos.forEach(function(todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  document.querySelector('body').appendChild(p)
+  if(todo.completed === false) {
+    left++;
   }
-}
+})
 
-const getThingsToDo = function(todos) {
-  return todos.filter(function(todo) {
-    return !todo.completed
-  })
-} 
+const todosLeft = document.createElement('h2')
+todosLeft.textContent = `You have ${left} todos left`
+document.querySelector('body').appendChild(todosLeft)
 
-sortTodos(todos)
-console.log(todos)
+// const todos = [{
+//   text: 'Order cat food',
+//   completed: false
+// }, {
+//   text: 'Clean kitchen',
+//   completed: true
+// }, {
+//   text: 'Buy food',
+//   completed: true
+// }, {
+//   text: 'Do work',
+//   completed: false
+// }, {
+//   text: 'Exercise',
+//   completed: true
+// }]
 
-// console.log(getThingsToDo(todos))
 
-// deleteTodo(todos, 'buy food')
+// const sortTodos = function(todos) {
+//   todos.sort(function(a, b) {
+//     if(!a.completed && b.completed) {
+//       return -1
+//     } else if(!b.completed && a.completed) {
+//       return 1
+//     } else {
+//       return 0
+//     }
+//   })
+// }
+
+// const deleteTodo = function(todos, todoText) {
+//   const index = todos.findIndex(function(todo){
+//     return todo.text.toLowerCase() === todoText.toLowerCase()
+//   })
+  
+//   if(index > -1) {
+//     todos.splice(index, 1);
+//   }
+// }
+
+// const getThingsToDo = function(todos) {
+//   return todos.filter(function(todo) {
+//     return !todo.completed
+//   })
+// } 
+
+// sortTodos(todos)
 // console.log(todos)
+
+// // console.log(getThingsToDo(todos))
+
+// // deleteTodo(todos, 'buy food')
+// // console.log(todos)

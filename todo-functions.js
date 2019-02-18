3cbbadf2227775af3697dad2beb8b62bc5867153
Petru <peter.giurgiu@gmail.com>
2019-02-18 22:28:57 +0200

    65-refactor-challenge

---
+++ todo-functions.js
@@ -0,0 +1,49 @@
+const getSavedTodos = function() {
+  const todosJSON = localStorage.getItem('todos') 
+
+  if(todosJSON !== null) {
+    return JSON.parse(todosJSON)
+  } else {
+    return []
+  }
+}
+
+const saveTodos = function(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos))
+} 
+
+const generateTodoDOM = function(todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  return p
+}
+
+const generataSummaryDOM = function(incompleteTodos) {
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
+
+  return summary
+}
+
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+    return searchTextMatch && hideCompletedMatch
+  })
+
+  const incompleteTodos = filteredTodos.filter(function (todo) {
+      return !todo.completed
+  })
+
+  document.querySelector('#todos').innerHTML = ''
+
+  const summary = generataSummaryDOM(incompleteTodos)
+  document.querySelector('#todos').appendChild(summary)
+
+  filteredTodos.forEach(function (todo) {
+    const p = generateTodoDOM(todo)
+    document.querySelector('#todos').appendChild(p)
+  })
+}

f07aa72a8177ccf3fe2528b50da593cdaf55d35a
Petru <peter.giurgiu@gmail.com>
2019-02-18 22:43:43 +0200

    66-degugging-our-applications

--- todo-functions.js
+++ todo-functions.js
@@ -1,36 +1,26 @@
-const getSavedTodos = function() {
-  const todosJSON = localStorage.getItem('todos') 
+// Fetch existing todos from localStorage
+const getSavedTodos = function () {
+  const todosJSON = localStorage.getItem('todos')
 
-  if(todosJSON !== null) {
-    return JSON.parse(todosJSON)
+  if (todosJSON !== null) {
+      return JSON.parse(todosJSON)
   } else {
-    return []
+      return []
   }
 }
 
-const saveTodos = function(todos) {
+// Save todos to localStorage
+const saveTodos = function (todos) {
   localStorage.setItem('todos', JSON.stringify(todos))
-} 
-
-const generateTodoDOM = function(todo) {
-  const p = document.createElement('p')
-  p.textContent = todo.text
-  return p
 }
 
-const generataSummaryDOM = function(incompleteTodos) {
-  const summary = document.createElement('h2')
-  summary.textContent = `You have ${incompleteTodos.length} todos left`
-
-  return summary
-}
-
+// Render application todos based on filters
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
-    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-
-    return searchTextMatch && hideCompletedMatch
+      const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+      const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+  
+      return searchTextMatch && hideCompletedMatch
   })
 
   const incompleteTodos = filteredTodos.filter(function (todo) {
@@ -38,12 +28,23 @@ const renderTodos = function (todos, filters) {
   })
 
   document.querySelector('#todos').innerHTML = ''
-
-  const summary = generataSummaryDOM(incompleteTodos)
-  document.querySelector('#todos').appendChild(summary)
+  document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
 
   filteredTodos.forEach(function (todo) {
-    const p = generateTodoDOM(todo)
-    document.querySelector('#todos').appendChild(p)
+      document.querySelector('#todos').appendChild(generateTodoDOM(todo))
   })
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function (todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  return p
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function (incompleteTodos) {
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
+  return summary
 }
